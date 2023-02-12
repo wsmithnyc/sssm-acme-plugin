b680cb55bfd9310b7b759135f654b7f95bd2da3a
William Mallick (Windows) <wmallick@outlook.com>
2023-02-12 14:56:34 -0500

    Initial commit

---
+++ seaport_museum_acme_ticketing.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Acme Ticketing Plugin
+Plugin URI:
+description: >-
+a plugin for exchanging data with Acme Ticketing
+Version: 1.0
+Author: William Mallick
+Author URI: wmallick@outlook.com
+License: GPL2
+*/
