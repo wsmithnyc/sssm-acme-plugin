82593188d36d3dbcc84d9147c236f6413b5a0351
William Mallick (Windows) <wmallick@outlook.com>
2023-02-18 21:06:52 -0500

    Add composer and guzzle

--- seaport_museum_acme_ticketing.php
+++ seaport_museum_acme_ticketing.php
@@ -2,10 +2,17 @@
 /*
 Plugin Name: Acme Ticketing Plugin
 Plugin URI:
-description: >-
-a plugin for exchanging data with Acme Ticketing
+description: A plugin for reading data from Acme Ticketing: <a href='https://www.acmeticketing.com/' target='_blank'>https://www.acmeticketing.com</a>
 Version: 1.0
 Author: William Mallick
 Author URI: wmallick@outlook.com
 License: GPL2
 */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+
